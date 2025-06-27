6ac810067b637b379c40f21b8aed4ed0cfca425a
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2025-06-26 21:57:52 -0300

    Creating the sqlite3 DB configuration file

--- config/db.go
+++ config/db.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func InitDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./cotacoes.db")
+	if err != nil {
+		panic(err)
+	}
+
+	createTable := `
+	CREATE TABLE IF NOT EXISTS cotacoes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		bid TEXT,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	);`
+
+	if _, err := db.Exec(createTable); err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
