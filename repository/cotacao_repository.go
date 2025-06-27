ed8e6d8cabdd5ab483d1ff669e738bdb63933f90
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2025-06-26 22:02:49 -0300

    Creation of cotacao_repository

--- repository/cotacao_repository.go
+++ repository/cotacao_repository.go
@@ -1,1 +1,33 @@
 package repository
+
+import (
+	"Cotacao/model"
+	"context"
+	"database/sql"
+	"log"
+	"time"
+)
+
+type CotacaoRepository interface {
+	SalvarCotacao(ctx context.Context, c model.Cotacao) error
+}
+
+type cotacaoRepository struct {
+	db *sql.DB
+}
+
+func NewCotacaoRepository(db *sql.DB) CotacaoRepository {
+	return &cotacaoRepository{db: db}
+}
+
+func (r *cotacaoRepository) SalvarCotacao(ctx context.Context, c model.Cotacao) error {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+	defer cancel()
+
+	query := "INSERT INTO cotacoes (bid) VALUES (?)"
+	_, err := r.db.ExecContext(ctx, query, c.Bid)
+	if err != nil {
+		log.Println("Erro ao salvar no banco:", err)
+	}
+	return err
+}
